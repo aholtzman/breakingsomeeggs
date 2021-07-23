8778c8f9667a11829debb6887ae52bff8af66bf8
aholtzman <adam.holtzman@gmail.com>
2021-07-23 18:06:27 -0400

    add egg

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import * as React from "react"
+import eggs from "../images/egg.png"
 // import { Link } from "gatsby"
 // import { StaticImage } from "gatsby-plugin-image"
 //
@@ -6,8 +7,11 @@ import * as React from "react"
 // import Seo from "../components/seo"
 
 const IndexPage = () => (
-  <div style={{ width: "100vw", height: "100vh", display: "flex", justifyContent: "center"}}>
+  <div style={{ width: "100vw", height: "100vh", display: "flex", justifyContent: "space-around", flexDirection: "column"}}>
+  <div style={{ width: "100vw", display: "flex", justifyContent: "space-around", flexDirection: "column"}}>
+    <img src={eggs} alt="cracked open egg" style={{ width: "400px", margin: "auto"}}/>
     <h1 style={{ margin: "auto", textAlign: "center"}}>breaking some eggs</h1>
+    </div>
   </div>
 )
 
