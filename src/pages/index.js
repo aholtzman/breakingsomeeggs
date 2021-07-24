fb116ce5129f5bbe9ed2b64ea701a43bc5a0a55c
aholtzman <adam.holtzman@gmail.com>
2021-07-24 13:42:24 -0400

    add layout and seo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,18 @@
 import * as React from "react"
 import eggs from "../images/egg.png"
-// import { Link } from "gatsby"
-// import { StaticImage } from "gatsby-plugin-image"
-//
-// import Layout from "../components/layout"
-// import Seo from "../components/seo"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <div style={{ width: "100vw", height: "100vh", display: "flex", justifyContent: "space-around", flexDirection: "column"}}>
-  <div style={{ width: "100vw", display: "flex", justifyContent: "space-around", flexDirection: "column"}}>
-    <img src={eggs} alt="cracked open egg" style={{ width: "400px", margin: "auto"}}/>
-    <h1 style={{ margin: "auto", textAlign: "center"}}>breaking some eggs</h1>
+  <Layout>
+    <SEO title="Breaking Some Eggs"/>
+    <div style={{ width: "100vw", height: "100vh", display: "flex", justifyContent: "space-around", flexDirection: "column"}}>
+    <div style={{ width: "100vw", display: "flex", justifyContent: "space-around", flexDirection: "column"}}>
+      <img src={eggs} alt="cracked open egg" style={{ width: "400px", margin: "auto"}}/>
+      <h1 style={{ margin: "auto", textAlign: "center"}}>breaking some eggs</h1>
+      </div>
     </div>
-  </div>
+  </Layout>
 )
 
 export default IndexPage
