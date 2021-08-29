15be89cd3913692e60b6dc322d6047c8bd1e437b
aholtzman <adam.holtzman@gmail.com>
2021-08-29 19:18:27 -0400

    fix page link

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,7 +13,7 @@ const About = () => (
     <p>The art here is the hustle. Attention is now a commodity; creatives are the labor providing products for insatiable online audiences feeding corporate bottom lines. The website, content, merch, and calls for support are all components of the project.</p>
     <p>There will also be photographs to buy if you need some scrambled eggs for your wall to feel complete.</p>
     <h2>Who is behind this?</h2>
-    <p>My name is Adam Holtzman. I make other things as well that you can see on my other <a href="https:adamholtzman.com" target="_blank" rel="noopener noreferrer" style={{ color: "black" }}>website</a>.</p>
+    <p>My name is Adam Holtzman. I make other things as well that you can see on my other <a href="https://adamholtzman.com" target="_blank" rel="noopener noreferrer" style={{ color: "black" }}>website</a>.</p>
   </Layout>
 )
 
